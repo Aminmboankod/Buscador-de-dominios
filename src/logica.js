f837ad0ac8477b46288ceda392dc86da7a209b3c
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-01-27 13:17:21 +0100

    fix(bug in function consultar)

--- src/logica.js
+++ src/logica.js
@@ -7,10 +7,9 @@ function consulta() {
     xhttp.onload = function() {
         if(this.responseText.length>0){  
             let data = JSON.parse(this.responseText);
-            let expiration = data.events[1];
-            document.getElementById("resultado").innerHTML = 
             document.getElementById("resultado").innerHTML = "El dominio " +data.ldhName + " está registrado.";
-            document.getElementById("fecha-expiracion").innerHTML = "La fecha de expiración es: " + expiration;
+            document.getElementById("fecha-expiracion").innerHTML = "La fecha de expiración es: " + data.events[1].eventDate;
+            document.getElementById("año-registrado").innerHTML = "El año de registro es: "+ data.events[0].eventDate;
         } else { document.getElementById("resultado").innerHTML = "El dominio " + dominio + " está libre.";
         }
     }
@@ -18,24 +17,3 @@ function consulta() {
     xhttp.open("GET", "https://rdap.verisign.com/com/v1/domain/"+dominio);
     xhttp.send();
 }
-
-
-// function query() {
-//         let form = document.querySelector('form');
-//     let results = document.querySelector('.results');
-//     let fechaRegistro = document.querySelector('#fecha-registro');
-//     let fechaExpiracion = document.querySelector('#fecha-expiracion');
-//     let anyoRegistrado = document.querySelector('#anyo-registrado');
-
-//     form.addEventListener('submit', e => {
-//         e.preventDefault();
-//         let dominio = document.getElementById('search').value;
-
-//         // llamada a la api aqui
-
-//         results.style.display = 'block';
-//         fechaRegistro.innerHTML = `Fecha de registro: ${fechaRegistro}`;
-//         fechaExpiracion.innerHTML = `Fecha de expiración: ${fechaExpiracion}`;
-//         anyoRegistrado.innerHTML = `Año registrado: ${anyoRegistrado}`;
-//     });
-// }
